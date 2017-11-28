510673219e092b2ed462b9950cb92e733a954362
noReasonException <stefan1998xd@gmail.com>
2017-11-28 22:36:07 +0200

    Inital Controllers(Rest API)

---
+++ API/ApiProject/ApiProject/Controllers/ActivitiesController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProject.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Activities")]
+    public class ActivitiesController : Controller
+    {
+    }
+}
