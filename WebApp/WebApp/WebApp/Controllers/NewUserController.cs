c68cc8eaa91e00a74bd352a0753fafbb932e0066
noReasonException <stefan1998xd@gmail.com>
2017-11-30 23:44:43 +0200

    Create New Customer Initial Code(Doc Pending..)

---
+++ WebApp/WebApp/WebApp/Controllers/NewUserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApp.Controllers
+{
+    public class NewUserController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            if (Request.Method == "POST")
+            {
+                ViewBag.Success = PostNew();
+            }
+            return View();
+        }
+        [NonAction]
+        public bool PostNew()
+        {
+            return true;
+        }
+        public bool CheckPostRequest()
+        {
+
+        }
+    }
+}
