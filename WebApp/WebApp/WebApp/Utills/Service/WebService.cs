8e97bb3996a6bef0131b2831da14d8cdb63ef20f
noReasonException <stefan1998xd@gmail.com>
2017-11-30 20:26:32 +0200

    Doc Added , Old GetAllRecords(with System.String Parameterr Removed)

--- WebApp/WebApp/WebApp/Utills/Service/WebService.cs
+++ WebApp/WebApp/WebApp/Utills/Service/WebService.cs
@@ -5,31 +5,48 @@ using System.Threading.Tasks;
 using WebApp.Utills;
 namespace WebApp.Utills.Service
 {
+
+    /// <summary>
+    /// This Class Is A Wrapper Over WebAPI Project , To Retrieve Easily Information 
+    /// uses the <see cref="Utills.RequestUtills.RequestUtills"/>
+    /// 
+    /// </summary>
     public class WebService
     {
-        static System.String ServiceUrl;
+        /// <summary>
+        /// Remember! Always Set <see cref="WebService.ServiceUrl"/> Attritude before use , OtherWise <see cref="InvalidOperationException"/> will thrown by every call!
+        /// </summary>
+        public static System.String ServiceUrl;
         static WebService()
         {
             ServiceUrl = System.String.Empty;
             
         }
-        
-
-        public static System.String GetAllRecords(System.String ServiceLocation)
-        {
-            return WebApp.Utills.RequestUtills.Get(ServiceLocation + "/api/Customers");
-
-        }
+        /// <summary>
+        /// Checks if <see cref="WebService.ServiceUrl"/> Attritide is Set! throws <see cref="InvalidOperationException"/> otherwise
+        /// </summary>
         private static void Check()
         {
             if (ServiceUrl.Equals(System.String.Empty)) throw new InvalidOperationException("Set the WebService.ServiceUrl First!");
             
         }
-        public static System.String GetAllRecords() 
+        /// <summary>
+        /// Get all records using the Rest WebApi 
+        /// Calls GET <urlOfService>/api/Customers 
+        /// 
+        /// </summary>
+        /// <returns> The Responce as  <see cref="System.String"/> Object</returns>
+        public static System.String GetAllRecords()
         {
             Check();
             return WebApp.Utills.RequestUtills.Get(WebService.ServiceUrl + "/api/Customers");
         }
+        /// <summary>
+        /// Get Record by ID (With Activities Included!)
+        /// Calls GET <urlOfService>/api/Customers/{Id}
+        /// </summary>
+        /// <param name="Id"> The ID of customer</param>
+        /// <returns> The Responce as  <see cref="System.String"/> Object</returns>
 
         public static System.String GetRecordByID(int Id)
         {
